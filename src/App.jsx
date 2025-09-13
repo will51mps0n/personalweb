24aa0571e3dd372a41853885ddaac90e7998766c
Adam <adwisi@umich.edu>
2025-09-13 10:59:45 -0500

    reworking with tailwind and react for better interface

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+           <div>
+            <h1>Welcome to my portfolio! Im adam</h1>
+            </div> 
+        </main>
+        
+      );
+}
+ 
+export default App;
