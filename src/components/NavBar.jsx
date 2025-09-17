052b4e9d1bf2115bdc25103a70085d13ffc4aef0
Adam <adwisi@umich.edu>
2025-09-17 12:08:29 -0500

    working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Adam Simpson
+          Adam Simpson 
         </a>
 
         <nav className="desktop">
